478ba156ff33c8ba10eac194b7cc9f8c281931a1
amroalbarham <amroalbarham@gmil.com>
2021-08-18 08:53:46 +0300

    hope work

--- src/context/settingsContext.js
+++ src/context/settingsContext.js
@@ -1,32 +1,38 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 export const SettingContext = React.createContext();
 
 function SettingsContext(props) {
-    const [numberOfItems,setNumberOfItems] = useState(3);
-    const [view,setView] = useState(false);
-    const [start,setStart] = useState(0);
-    const [end,setEnd] = useState(numberOfItems-1);
+
+  const [numberOfItems, setNumberOfItems] = useState(3);
+  const [view, setView] = useState(false);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(numberOfItems - 1);
 
 
-    function setSettings(numb,val){
-            setNumberOfItems(numb)
+  function setSettings(numb, val) {
+      setNumberOfItems(numb)
+      setView(val);
 
-            setView(val); 
-    }
-   function nextpage(){
-       setStart(start+numberOfItems);
-       setEnd(end+numberOfItems);
-   }
-   function previouspage(){
-       setStart(start-numberOfItems);
-       setEnd(end-numberOfItems);
-   }
-    return (
-        <SettingContext.Provider value={{numberOfItems,view,setSettings,start,end,nextpage,previouspage}}>
-            {props.children}
-        </SettingContext.Provider>
-    )
+  }
+
+  function nextpage() {
+    setStart(start + numberOfItems);
+    setEnd(end + numberOfItems);
+
+  }
+
+  function previouspage() {
+    setStart(start - numberOfItems);
+    setEnd(end - numberOfItems);
+  }
+
+  return (
+    <SettingContext.Provider value={{ numberOfItems, view, setSettings, start, end, nextpage, previouspage }}>
+      {props.children}
+    </SettingContext.Provider>
+  )
+  
 }
 
 export default SettingsContext
